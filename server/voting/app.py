8379cd7b55e97215283802c6dd5fd3428ef1fcf5
Rishabh Kanojiya <rishabhkanojiya75@gmial.com>
2024-02-18 00:33:27 -0500

    Added comment

--- server/voting/app.py
+++ server/voting/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,  request, jsonify
+from flask import Flask, request, jsonify
 import psycopg2
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -6,11 +6,13 @@ import os
 
 load_dotenv()
 
+# postgres://root:root@internal-postgres-svc:5432/be-e-voting
 postgres_uri = os.getenv("POSTGRES_BILL_SPLIT_READ_WRITE")
 
 print("host", postgres_uri)
 app = Flask(__name__)
 CORS(app)
+
 
 def connect_db():
     try:
@@ -25,10 +27,12 @@ def connect_db():
 conn = connect_db()
 cursor = conn.cursor()
 
-@app.route('/create_election_candidates_table', methods=['POST'])
+
+@app.route("/create_election_candidates_table", methods=["POST"])
 def create_election_candidates_table():
     try:
-        cursor.execute('''
+        cursor.execute(
+            """
             CREATE TABLE IF NOT EXISTS ELECTION_CANDIDATES (
                 candidate_id SERIAL PRIMARY KEY,
                 candidate_name VARCHAR NOT NULL,
@@ -38,17 +42,23 @@ def create_election_candidates_table():
                 candidate_location VARCHAR,
                 candidate_creation_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             )
-        ''')
+        """
+        )
         conn.commit()
-        return jsonify({'message': 'Table created successfully'}), 200
+        return jsonify({"message": "Table created successfully"}), 200
     except Exception as e:
         conn.rollback()
-        return jsonify({'error': f"Error creating table 'ELECTION_CANDIDATES': {str(e)}"}), 500
-    
-@app.route('/create_election_table', methods=['POST'])
+        return (
+            jsonify({"error": f"Error creating table 'ELECTION_CANDIDATES': {str(e)}"}),
+            500,
+        )
+
+
+@app.route("/create_election_table", methods=["POST"])
 def create_election_table():
     try:
-        cursor.execute('''
+        cursor.execute(
+            """
             CREATE TABLE IF NOT EXISTS ELECTION (
                 election_id SERIAL PRIMARY KEY,
                 election_name VARCHAR,
@@ -58,17 +68,23 @@ def create_election_table():
                 election_state VARCHAR,
                 election_city VARCHAR
             )
-        ''')
+        """
+        )
         conn.commit()
-        return jsonify({'message': 'Table created successfully'}), 200
+        return jsonify({"message": "Table created successfully"}), 200
     except Exception as e:
         conn.rollback()
-        return jsonify({'error': f"Error creating table 'ELECTION_CANDIDATES': {str(e)}"}), 500
+        return (
+            jsonify({"error": f"Error creating table 'ELECTION_CANDIDATES': {str(e)}"}),
+            500,
+        )
 
-@app.route('/create_voting_information_table', methods=['POST'])
+
+@app.route("/create_voting_information_table", methods=["POST"])
 def create_voting_information_table():
     try:
-        cursor.execute('''
+        cursor.execute(
+            """
             CREATE TABLE IF NOT EXISTS VOTING_INFORMATION (
                 voting_id SERIAL PRIMARY KEY,
                 voter_ssn INT NOT NULL,
@@ -78,12 +94,16 @@ def create_voting_information_table():
                 FOREIGN KEY (voting_election_id) REFERENCES ELECTION(election_id),
                 FOREIGN KEY (voting_candidate_id) REFERENCES ELECTION_CANDIDATES(candidate_id)
             )
-        ''')
+        """
+        )
         conn.commit()
-        return jsonify({'message': 'Table created successfully'}), 200
+        return jsonify({"message": "Table created successfully"}), 200
     except Exception as e:
         conn.rollback()
-        return jsonify({'error': f"Error creating table 'ELECTION_CANDIDATES': {str(e)}"}), 500
+        return (
+            jsonify({"error": f"Error creating table 'ELECTION_CANDIDATES': {str(e)}"}),
+            500,
+        )
 
 
 if __name__ == "__main__":
