86381e0a1a2458474eac129e5b8e506b471f7bd8
Rishabh Kanojiya <rishabhkanojiya75@gmial.com>
2024-02-17 22:38:08 -0500

    Minor change

--- server/voting/app.py
+++ server/voting/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import psycopg2
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
@@ -9,62 +10,60 @@ postgres_uri = os.getenv("POSTGRES_BILL_SPLIT_READ_WRITE")
 
 print("host", postgres_uri)
 app = Flask(__name__)
+CORS(app)
 
 
-# Connect to PostgreSQL
-def connect_db():
-    try:
-        conn = psycopg2.connect(postgres_uri)
-        print("Connected to the database successfully.")
-        return conn
-    except psycopg2.OperationalError as e:
-        print(f"Unable to connect to the database. Error: {e}")
-        raise e
+# # Connect to PostgreSQL
+# def connect_db():
+#     try:
+#         conn = psycopg2.connect(postgres_uri)
+#         print("Connected to the database successfully.")
+#         return conn
+#     except psycopg2.OperationalError as e:
+#         print(f"Unable to connect to the database. Error: {e}")
+#         raise e
 
 
-# Initialize the database table
-def init_db():
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS tasks (
-            id SERIAL PRIMARY KEY,
-            description TEXT NOT NULL
-        )
-    """
-    )
-    conn.commit()
-    conn.close()
+# # Initialize the database table
+# def init_db():
+#     conn = connect_db()
+#     cursor = conn.cursor()
+#     cursor.execute(
+#         """
+#         CREATE TABLE IF NOT EXISTS tasks (
+#             id SERIAL PRIMARY KEY,
+#             description TEXT NOT NULL
+#         )
+#     """
+#     )
+#     conn.commit()
+#     conn.close()
 
 
 # Home route to display tasks
+# v1.0/user/me
 @app.route("/")
 def index():
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM tasks ORDER BY id")
-    tasks = cursor.fetchall()
-    conn.close()
-    return render_template("index.html", tasks=tasks)
+    data = {"message": "Hello, this is a JSON response!", "status": "success"}
+    return jsonify(data)
 
 
-# Add new task route
-@app.route("/add", methods=["POST"])
-def add():
-    new_task = request.form["task"]
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO tasks (description) VALUES (%s)", (new_task,))
-    conn.commit()
-    conn.close()
-    return redirect(url_for("index"))
+# # Add new task route
+# @app.route("/add", methods=["POST"])
+# def add():
+#     new_task = request.form["task"]
+#     conn = connect_db()
+#     cursor = conn.cursor()
+#     cursor.execute("INSERT INTO tasks (description) VALUES (%s)", (new_task,))
+#     conn.commit()
+#     conn.close()
+#     return redirect(url_for("index"))
 
 
 # Run the application
 if __name__ == "__main__":
     # Initialize the database
-    init_db()
+    # init_db()
 
     # Run the Flask app
-    app.run(debug=True)
+    app.run(port=9069)
